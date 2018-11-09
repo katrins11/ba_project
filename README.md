a89c217d6373a05c0ad838d146a8bdbd368acf8f
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-11-09 14:20:51 +0100

    setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ba_project
+
+<!-- For running the postCss  -->
+npm run postcss
+
+<!-- For watching the postCss  -->
+npm run postcss-watch
--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@ const gulp = require("gulp")
 const postcss = require("gulp-postcss")
 const autoprefixer = require("autoprefixer")
 const responsiveType = require("postcss-responsive-type")
+const lost = require("lost");
+const precss = require("precss");
+const cssnext = require("postcss-responsive-type");
+const postcssImport = require("postcss-import");
 
 gulp.task("css", function() {
-  const processors = [autoprefixer, responsiveType]
+  const processors = [
+    precss, 
+    cssnext, 
+    lost, 
+    autoprefixer({browsers:['last 2 version']}), 
+    responsiveType,
+    postcssImport
+  ]
   return gulp
-    .src("./src/*.css")
+    .src("./src/CSS/styles.css")
     .pipe(postcss(processors))
     .pipe(gulp.dest("./build"))
 });
 
 gulp.task("watch", function() {
-  gulp.watch("./src/*", ["css"])
+  gulp.watch("./src/CSS/*", ["css"])
 })
