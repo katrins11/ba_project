29be3beb1f2f77b135b58087fe26455426e3a479
Anne Saerker <annemai11@hotmail.com>
2018-11-15 11:04:52 +0100

    simple grid setup frontpage

--- build/index.php
+++ build/index.php
@@ -46,6 +46,19 @@
         </section>
 
         <section>
+            <div class="newest-posts">
+                <h1 class="section-headline">
+                    Newest internship oppotunities
+                </h1>
+                <div class="card-container">
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                </div>
+            </div>
+        </section>
+
+        <section>
             <div class="cta">
                 <h1 class="section-headline">
                  see more oppotunities
