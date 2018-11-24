b0a24cb748f111565ca8f8bcd8393fca7f70ec6c
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-11-24 10:13:30 +0100

    images into database for UMWELT

--- src/index.php
+++ src/index.php
@@ -37,9 +37,13 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"></script> -->
 
-        <!-- database -->
+        <!-- Database -->
         <script src="https://www.gstatic.com/firebasejs/5.5.9/firebase.js"></script>
         <script src="js/data.js"></script>
+
+        <!-- Search -->
+        <script src="js/mixitup.min.js"></script>
+        <script src="js/search.js"></script>
 
     </body>
 </html>
--- src/search.php
+++ src/search.php
@@ -54,37 +54,5 @@
       <a href="#">The Namesake</a>
     </div>
   </div>
-
-  <!-- Filtering -->
-  <script src="js/mixitup.min.js"></script>
-  <script>
-    // Search
-    let filterInput = document.getElementById('filter-input');
-    // Add event listener
-    filterInput.addEventListener('keyup', filterNames);
-
-    function filterNames(){
-      // Get value of input
-      let filterValue = document.getElementById('filter-input').value.toLowerCase();
-      // Get names ul
-      let Allposts = document.getElementById('resultscontainer');
-      // Get lis from ul
-      let postItem = Allposts.querySelectorAll('.eachPost');
-      // Loop through collection-item lis
-      for(let i = 0;i < postItem.length;i++){
-        let a = postItem[i].getElementsByTagName('a')[0];
-        // If matched
-        if(a.innerHTML.toLowerCase().indexOf(filterValue) > -1){
-          postItem[i].style.display = '';
-        } else {
-          postItem[i].style.display = 'none';
-        }
-      }
-    }
-    
-    //Filtering
-    var containerEl = document.querySelector('.resultscontainer');
-    var mixer = mixitup(containerEl);
-  </script>
-
+  
 </section>
