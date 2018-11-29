4aa4c6b3a8d88c139edae8816e2cd5b6021c1cbc
Anne Saerker <annemai11@hotmail.com>
2018-11-29 10:34:57 +0100

    post page structure change

--- src/overview.php
+++ src/overview.php
@@ -1,11 +1,10 @@
-        <?php include('php/head.php')?>
+<?php include('php/head.php')?>
 
-        <?php include('php/header.php')?>
-        <?php include('php/menu.php')?>
+<?php include('php/header.php')?>
+<?php include('php/menu.php')?>
 
-        <?php include('php/search.php')?>
+<?php include('php/search.php')?>
 
-        <?php include('php/footer.php')?>
+<?php include('php/footer.php')?>
 
-    </body>
-</html>
+<?php include('php/foot.php')?>
--- src/post.php
+++ src/post.php
@@ -1,36 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Web Intern</title>
-        
-        <!-- css -->
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-    </head>
-    <body>
-        <?php include('header.php')?>
+<?php include('php/head.php')?>
 
-        <?php include('post-hero.php')?>
-        
-        <?php include('info-box.php')?>
+<?php include('php/header.php')?>
 
-        <?php include('paralax-post-text.php')?>
+<?php include('php/post-hero.php')?>
 
-        <?php include('cta.php')?>
+<?php include('php/info-box.php')?>
 
-        <?php include('footer.php')?>
-            
-        <!-- animations -->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/2.0.0/TweenMax.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script> 
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<?php include('php/paralax-post-text.php')?>
 
-        <!-- scripts -->
-        <!-- <script src="js/script.js"></script> -->
-        <script src="js/paralax-post-text.js"></script>
-    </body>
-</html>
+<?php include('php/slider.php')?>
+
+<?php include('php/cta.php')?>
+
+<?php include('php/footer.php')?>
+
+<?php include('php/foot.php')?>
