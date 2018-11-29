99e184759b84fd3b5904847608f68b7e696bd144
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-11-29 19:05:57 +0100

    search and cards ready, just the filtering left for when getting data from database. Works for the code commented out in search.php

--- src/php/scripts.php
+++ src/php/scripts.php
@@ -18,12 +18,12 @@
 <script src="js/shape-animation.js"></script>
 
 <!-- ScrollMagic-->
-<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
+<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/gsap/2.0.0/TweenMax.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script> 
 <script src="js/paralax-frontpage.js"></script>
-<script src="js/paralax-post-text.js"></script>
+<script src="js/paralax-post-text.js"></script> -->
 <!-- <script src="js/paralax-post-contact.js"></script> -->
 
 <!-- Script -->
