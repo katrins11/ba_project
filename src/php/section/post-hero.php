952dfe82413f4ccbe841febd389aecc37c14612f
Anne Saerker <annemai11@hotmail.com>
2018-11-29 16:14:09 +0100

    working on info box's on post page

--- src/php/section/post-hero.php
+++ src/php/section/post-hero.php
@@ -4,3 +4,21 @@
     <h1>start your future now</h1>
   </div>
 </section>
+<section class="info-box hero-info-box">
+  <div class="info">
+    <h3>01.02.19</h3>
+    <p>Deadline</p>
+  </div>
+  <div class="info">
+    <h3>3 months</h3>
+    <p>Duration</p>
+  </div>
+  <div class="info">
+    <h3>Copenhagen</h3>
+    <p>Location</p>
+  </div>
+  <div class="info">
+    <h3>22.03.19</h3>
+    <p>Date</p>
+  </div>
+</section>
