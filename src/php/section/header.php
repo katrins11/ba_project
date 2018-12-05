d51d092b67828dfe46406cd54c8b464da5368cf9
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-12-05 11:23:42 +0100

    heroBanner with animation

--- src/php/section/header.php
+++ src/php/section/header.php
@@ -6,7 +6,6 @@
 
   <nav class="menu-btn" id="btnmenu">
       <p>menu</p>
-      <p>close</p>
       <div class="line"></div>
       <div class="line"></div>
       <div class="line"></div>
@@ -31,19 +30,19 @@
     }
 
      //burger to x
-    var menu = document.getElementById("btnmenu");
-    var navigation = document.getElementById("navigation");
+    // var menu = document.getElementById("btnmenu");
+    // var navigation = document.getElementById("navigation");
 
-    menu.onclick = function() {
-        if(menu.classList.contains("menu-active")) {
-            menu.classList.remove("menu-active");
-            navigation.classList.remove("nav-active");
-            return;
-        }
-        else {
-            menu.classList.add("menu-active");
-            navigation.classList.add("nav-active");
-        }
-    };
+    // menu.onclick = function() {
+    //     if(menu.classList.contains("menu-active")) {
+    //         menu.classList.remove("menu-active");
+    //         navigation.classList.remove("nav-active");
+    //         return;
+    //     }
+    //     else {
+    //         menu.classList.add("menu-active");
+    //         navigation.classList.add("nav-active");
+    //     }
+    // };
   </script>
 </header>
--- src/php/section/hero-banner.php
+++ src/php/section/hero-banner.php
@@ -1,26 +1,22 @@
 <section class="hero-banner">
-  <div class="hero-overlay"></div>
-  <div class="tag-line">
-    <h1>start your future now</h1>
-  </div>
 
-<div>
-  <svg class="triangle spin one" width="282" height="233" viewBox="0 0 282 233" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M140.969 2.78383L279.07 231.037L1.8358 230.409L140.969 2.78383Z" stroke="#FFE500" stroke-width="2"/>
-  </svg>
-
-  <svg class="triangle spin two" width="282" height="233" viewBox="0 0 282 233" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M140.969 2.78383L279.07 231.037L1.8358 230.409L140.969 2.78383Z" stroke="#FFE500" stroke-width="2"/>
-  </svg>
-
-  <svg class="triangle spin three"width="278" height="229" viewBox="0 0 278 229" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M139.134 0.314098L277.234 228.568L0.0001038 227.939L139.134 0.314098Z" fill="#FFE500"/>
-  </svg>
-</div>
-
-  <!-- <div class="content">
-    <svg class="scene" width="100%" height="100vh" preserveAspectRatio="none" viewBox="0 0 1440 800" xmlns:pathdata="http://www.codrops.com/">
-      <path d="M 1066,436 C 1051,543.8 973.2,656.2 873.6,700.1 756.6,751.7 600.9,725 492.4,657.4 431.5,619.5 387.5,546.9 376.7,476 360.3,368.3 377.9,229.2 462.5,160.5 589.5,57.34 815.4,42.24 952.4,131.7 1044,190.8 1081,328.8 1066,436 Z" pathdata:id="M 1041,450.4 C 1023,547.7 992.8,667.7 905.7,714.5 793.1,775 639,728.7 524.5,671.8 453.3,636.4 382.2,575.4 360.2,499 329.7,393 344.6,249.2 426,174.9 568.6,44.66 851.1,-8.71 1002,111.8 1091,182.7 1061,338.6 1041,450.4 Z;M 1066,436 C 1051,543.8 976.5,664.5 873.6,700.1 761,739.1 636.4,655.8 529.5,603.1 441.6,559.8 325.8,520.1 293.8,427.5 263.1,338.4 294.5,213.4 368.2,154.8 520.7,33.48 790.1,23.76 952.4,131.7 1043,191.7 1081,328.8 1066,436 Z;M 1066,436 C 1053,531.1 930.7,580.1 842.2,617.2 734,662.7 598.4,707.8 492.4,657.4 427.6,626.6 387.5,546.9 376.7,476 360.3,368.3 376.9,227.9 462.5,160.5 567.6,77.69 749.9,37.5 863.8,148.8 947.6,230.7 1082,320.1 1066,436 Z"/>
+  <div class="menu__logo">
+    <svg width="750px" height="748px" viewBox="0 0 750 748" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <defs>
+          <radialGradient id="main-gradient" cx="50%" cy="50%" fx="50%" fy="50%" r="51.1797663%" gradientTransform="translate(0.500000,0.500000),scale(0.997022,1.000000),rotate(-3.156509),translate(-0.500000,-0.500000)" id="radialGradient-1">
+              <stop stop-color="#FFFFFF" offset="0%"></stop>
+              <stop stop-color="#FFE500" offset="100%"></stop>
+          </radialGradient>
+      </defs>
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="Artboard" transform="translate(-489.000000, -26.000000)" fill="url(#radialGradient-1)" fill-rule="nonzero">
+              <g id="SVG-Layer" transform="translate(489.000000, 26.000000)">
+                  <path class="js-logo-shape" fill="url(#main-gradient)" d="M669.539822,77.2274228 C739.269513,143.514346 759.52104,253.762389 745.252919,364.010432 C730.984797,474.258475 681.966896,584.73668 612.697467,651.48393 C497.171708,762.882787 256.684821,782.676799 131.953823,681.635273 C-2.2585726,572.768208 -46.0985268,307.620514 56.9311188,168.832268 C118.376094,85.9736141 234.131983,27.5582628 353.339838,7.53408799 C472.317561,-12.4440542 594.747249,6.15311041 669.539822,77.2274228 Z" id="Shape"></path>
+              </g>
+          </g>
+      </g>
     </svg>
-  </div> -->
+
+    <h1 class="tag-line">Start your future now</h1>
+  </div>
 </section>
