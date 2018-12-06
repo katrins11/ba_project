59199ebe022788b64c6823a07050099664ae4c15
Anne Saerker <annemai11@hotmail.com>
2018-12-06 16:04:25 +0100

    front page text

--- src/php/section/frontPosts.php
+++ src/php/section/frontPosts.php
@@ -1,4 +1,4 @@
-<section class="frontPosts" style="width: 100vw;">
+<section class="frontPosts section-watch" style="width: 100vw;">
     <h3>Reacently added internships!</h3>
     <div class="frontPagePosts" id="frontPagePosts" style="width: 100%;"></div>
 </section>
