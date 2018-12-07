ee7a44ca9c0fa521255c031d5aa0ec350ec3c069
Anne Saerker <annemai11@hotmail.com>
2018-12-07 16:46:52 +0100

    started getting every card data

--- src/overview.php
+++ src/overview.php
@@ -1,30 +1,14 @@
 <?php include('php/head.php')?>
-    <div class="page" id="overview-page" data-overviewId="1">
-        <?php include('php/header.php')?>
-        <?php include('php/menu.php')?>
+  <div class="page" id="overview-page" data-overviewId="1">
+    <?php include('php/header.php')?>
+    <?php include('php/menu.php')?>
 
-        <?php include('php/search.php')?>
+    <?php include('php/search.php')?>
 
-        <div id="pop-up-modal" class="modal">
-            <div class="modal-content">
-                <span class="close">&times;</span>  
-                <?php include('php/hero-post.php')?>
+    <?php include('php/pop-up-modal.php')?>
 
-                <?php include('php/post-text.php')?>
-
-                <?php include('php/carousel.php')?>
-
-                <?php include('php/info-box.php')?>
-
-                <?php include('php/post-contact.php')?>
-                <section class="btn-close">
-                    <button>Close</button>
-                </section>
-            </div>
-        </div>
-
-        <?php include('php/carousel.php')?>
-        
-        <?php include('php/footer.php')?>
-    </div>
+    <?php include('php/carousel.php')?>
+    
+    <?php include('php/footer.php')?>
+  </div>
 <?php include('php/foot.php')?>
--- src/php/section/header.php
+++ src/php/section/header.php
@@ -22,7 +22,7 @@
 
     function myFunction() {
         if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
+            navbar.classList.add("sticky");
         } 
         else {
             navbar.classList.remove("sticky");
